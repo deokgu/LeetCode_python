9b3663875384366e4125d15dbfbd323986c0af1b
deokgu <onefence1994@gmail.com>
2021-09-24 00:04:46 +0900

    15

--- array/15_3Sum.py
+++ array/15_3Sum.py
@@ -8,5 +8,21 @@
     Output: [[-1,-1,2],[-1,0,1]]
 """
 
-def threeSum(nums: List[int]) -> List[List[int]]:
-    pass
+def threeSum(nums : list[int]): # -> list(list(int) 
+    if len(nums) < 3:
+        return []
+
+    for num in nums:
+        _list = []
+        _list.append(num)
+        _nums = nums[:]
+        _nums.remove(num)
+        i = 0
+        while i < len(_nums)-1:
+            _list.append(_nums[i])
+            
+
+            find_zero = nums[left] + nums[right]
+        
+if __name__ == "__main__":
+    print(threeSum([-1,0,1,2,-1,-4])) # [[-1,-1,2],[-1,0,1]]
