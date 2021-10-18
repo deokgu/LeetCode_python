a377e625ff73168024de98d3e9931f9bad3f12b0
mingu <onefence1994@gmail.com>
2021-10-18 11:07:48 +0900

    add Implementation 3

--- This_is_coding_test/Greedy/example/문자열_뒤집기.py
+++ This_is_coding_test/Greedy/example/문자열_뒤집기.py
@@ -1,15 +1,15 @@
 
 # s = input()
 # s  = "0001100"
-s  = "10101010111001"
+s  = "11001100110011000001"
 
 temp = s[0]
-_count = {temp: 1}
+_count = {temp: 1, str(int(temp) ^ 1): 0}
 for x in s[1:]:
     if temp == x:
         continue
     else:
         temp = x
-        _count[x] = _count.get(x, 0) + 1
+        _count[x] += 1
 
 print(min(_count.values()))
